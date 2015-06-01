100413f0ad3254f1fd338021cec4c983c0094564
Sudhanshu Singh <Sudhanshu.Singh@makemytrip.com>
2015-06-01 23:28:51 +0530

    First Commit

---
+++ CRTouchIDWrapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CRTouchIDWrapper
+//
+//  Created by Sudhanshu Singh on 01/06/15.
+//  Copyright (c) 2015 CheeseRoot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
